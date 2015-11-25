a256f1fbcf6afcd6a973f7ca7a8c36fe0e333a2f
yoonju <cyj08070807@naver.com>
2015-11-25 15:27:19 +0900

    commit3

---
+++ app/src/main/java/com/example/bit_user/myapllication/core/JSONResult.java
@@ -0,0 +1,51 @@
+package com.example.bit_user.myapllication.core;
+
+public abstract class JSONResult<DataT> {
+	
+	private String result;
+	private String message;
+	private DataT data;
+	
+	public JSONResult() {
+	}
+	
+	public JSONResult( String result, String message, DataT data ) {
+		this.result = result;
+		this.message = message;
+		this.data = data;
+	}
+
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess(){
+		return "OK".equals( result );
+	}
+	
+	public DataT getData() {
+		return data;
+	}
+	
+	public void setData( DataT data ) {
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "JSONResult [result=" + result + ", message=" + message + ", data=" + data + "]";
+	}	
+}
